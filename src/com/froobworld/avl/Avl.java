0944b93ed04b8f8af0b54037e5b62a191bbdbddb
froobynooby <froobynooby@froobworld.com>
2019-06-20 15:52:00 +1000

    Add config

--- src/com/froobworld/avl/Avl.java
+++ src/com/froobworld/avl/Avl.java
@@ -8,11 +8,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public class Avl extends JavaPlugin {
+    private AvlConfiguration config;
 
     public void onEnable() {
         if(new CompatibilityCheckTask(this).passedCheck()) {
+            config = new AvlConfiguration(this, AvlConfiguration.CONFIG_CURRENT_VERSION, "config.yml");
             Bukkit.getPluginManager().registerEvents(new EventListener(this), this);
         }
+    }
+
+    public AvlConfiguration getAvlConfig() {
+        return config;
     }
 
     public static Logger logger() {
