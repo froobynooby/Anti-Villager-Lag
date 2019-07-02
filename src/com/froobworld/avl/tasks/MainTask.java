b5eee5943a4e79d07347435bafec5eca767ed38a
Rincewind34 <rincewind34@wtnet.de>
2019-07-02 14:43:43 +0200

    Fixed memory issue

--- src/com/froobworld/avl/tasks/MainTask.java
+++ src/com/froobworld/avl/tasks/MainTask.java
@@ -1,13 +1,21 @@
 package com.froobworld.avl.tasks;
 
-import com.froobworld.avl.Avl;
 import org.bukkit.Bukkit;
+
+import com.froobworld.avl.Avl;
 
 public class MainTask implements Runnable {
     private Avl avl;
+    
+    private final NormalActivityTask activityTask;
+    private final RemoveActivityTask removeTask;
 
     public MainTask(Avl avl) {
         this.avl = avl;
+        
+        this.activityTask = new NormalActivityTask();
+        this.removeTask = new RemoveActivityTask();
+        
         run();
     }
 
@@ -16,7 +24,7 @@ public class MainTask implements Runnable {
         long ticksPerAllowSearch = avl.getAvlConfig().getLong("ticks-per-allow-search");
         Bukkit.getScheduler().scheduleSyncDelayedTask(avl, this, ticksPerAllowSearch <= 0 ? 600 : ticksPerAllowSearch);
 
-        new NormalActivityTask().run();
-        Bukkit.getScheduler().scheduleSyncDelayedTask(avl, new RemoveActivityTask(), 1);
+        this.activityTask.run();
+        Bukkit.getScheduler().scheduleSyncDelayedTask(avl, this.removeTask, 1);
     }
 }
