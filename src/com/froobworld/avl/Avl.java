74262d9e2628aba08a353d2e61d72814f9ce0476
Rincewind34 <rincewind34@wtnet.de>
2019-07-02 14:48:59 +0200

    Using timer task instead of recursive delayed task

--- src/com/froobworld/avl/Avl.java
+++ src/com/froobworld/avl/Avl.java
@@ -2,6 +2,7 @@ package com.froobworld.avl;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.froobworld.avl.metrics.Metrics;
@@ -9,30 +10,32 @@ import com.froobworld.avl.tasks.CompatibilityCheckTask;
 import com.froobworld.avl.tasks.MainTask;
 
 public class Avl extends JavaPlugin {
-    private AvlConfiguration config;
+	private AvlConfiguration config;
 
-    @Override
+	@Override
 	public void onEnable() {
-        if(new CompatibilityCheckTask(this).passedCheck()) {
-            config = new AvlConfiguration(this, AvlConfiguration.CONFIG_CURRENT_VERSION, "config.yml");
-            config.loadFromFile();
+		if (new CompatibilityCheckTask(this).passedCheck()) {
+			config = new AvlConfiguration(this, AvlConfiguration.CONFIG_CURRENT_VERSION, "config.yml");
+			config.loadFromFile();
 
-            addTasks();
+			addTasks();
 
-            new Metrics(this);
-            logger().info("Successfully enabled.");
-        }
-    }
+			new Metrics(this);
+			logger().info("Successfully enabled.");
+		}
+	}
 
-    private void addTasks() {
-        new MainTask(this);
-    }
+	private void addTasks() {
+		long ticksPerAllowSearch = config.getLong("ticks-per-allow-search",
+				600L /* Default value, if config does not contain the entry */);
+		Bukkit.getScheduler().runTaskTimer(this, new MainTask(this), 0L, ticksPerAllowSearch <= 0 ? 600 : ticksPerAllowSearch);
+	}
 
-    public AvlConfiguration getAvlConfig() {
-        return config;
-    }
+	public AvlConfiguration getAvlConfig() {
+		return config;
+	}
 
-    public static Logger logger() {
-        return JavaPlugin.getPlugin(Avl.class).getLogger();
-    }
+	public static Logger logger() {
+		return JavaPlugin.getPlugin(Avl.class).getLogger();
+	}
 }
--- src/com/froobworld/avl/tasks/MainTask.java
+++ src/com/froobworld/avl/tasks/MainTask.java
@@ -21,9 +21,6 @@ public class MainTask implements Runnable {
 
     @Override
     public void run() {
-        long ticksPerAllowSearch = avl.getAvlConfig().getLong("ticks-per-allow-search");
-        Bukkit.getScheduler().scheduleSyncDelayedTask(avl, this, ticksPerAllowSearch <= 0 ? 600 : ticksPerAllowSearch);
-
         this.activityTask.run();
         Bukkit.getScheduler().scheduleSyncDelayedTask(avl, this.removeTask, 1);
     }
