8cc861ecfb6fd3f9593110a43789bf7373849a39
Rincewind34 <rincewind34@wtnet.de>
2019-07-02 14:51:18 +0200

    Fixed resource memory leaks

--- src/com/froobworld/avl/utils/ConfigUpdater.java
+++ src/com/froobworld/avl/utils/ConfigUpdater.java
@@ -1,49 +1,55 @@
 package com.froobworld.avl.utils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigUpdater {
-    public static boolean update(File configFile, InputStream patchInputStream, int versionFrom) {
-        try {
-            BufferedReader configReader = new BufferedReader(new FileReader(configFile));
+	public static boolean update(File configFile, InputStream patchInputStream, int versionFrom) {
+		List<String> lines = new ArrayList<>();
 
-            List<String> lines = new ArrayList<String>();
-            String line = configReader.readLine();
-            while(line != null) {
-                if(line.startsWith("version:")) {
-                    line = line.replaceFirst("version: " + versionFrom, "version: " + (versionFrom + 1));
-                }
-                lines.add(line);
-                line = configReader.readLine();
-            }
-            configReader.close();
+		try (BufferedReader configReader = new BufferedReader(new FileReader(configFile))) {
+			String line = configReader.readLine();
+			while (line != null) {
+				if (line.startsWith("version:")) {
+					line = line.replaceFirst("version: " + versionFrom, "version: " + (versionFrom + 1));
+				}
+				lines.add(line);
+				line = configReader.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 
-            PrintWriter writer = new PrintWriter(new FileWriter(configFile));
+		try (PrintWriter writer = new PrintWriter(new FileWriter(configFile))) {
+			for (String string : lines) {
+				writer.println(string);
+			}
 
-            for(String string : lines) {
-                writer.println(string);
-            }
+			try (BufferedReader patchReader = new BufferedReader(new InputStreamReader(patchInputStream))) {
+				writer.println("");
+				writer.println("");
+				writer.println("###### Added in config update " + versionFrom + " to " + (versionFrom + 1) + " ######");
+				String line = patchReader.readLine();
+				while (line != null) {
+					writer.println(line);
+					line = patchReader.readLine();
+				}
 
-            BufferedReader patchReader = new BufferedReader(new InputStreamReader(patchInputStream));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 
-            writer.println("");
-            writer.println("");
-            writer.println("###### Added in config update " + versionFrom + " to " + (versionFrom + 1) +  " ######");
-            line = patchReader.readLine();
-            while(line != null) {
-                writer.println(line);
-                line = patchReader.readLine();
-            }
-
-            patchReader.close();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
+		return true;
+	}
 }
